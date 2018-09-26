6c10e29a03d3ae413a596330c44d684f7ad2dd18
rodergstep <rodergstep@gmail.com>
2018-09-26 11:20:12 +0300

    added print styles

--- js/main.js
+++ js/main.js
@@ -1,110 +1,110 @@
 $(document).ready(function() {
-  $('.question').tooltip();
-  $('.tabs').tabs();
+  $('.question').tooltip()
+  $('.tabs').tabs()
   $('.modal').modal({
     opacity: 0.8
-  });
-  $('select').formSelect();
+  })
+  $('select').formSelect()
   $('.top-search__input input').keyup(function(e) {
     if ($(this).val().length) {
       $(this)
         .closest('.top-search__input')
         .find('.clear')
-        .fadeIn(100);
+        .fadeIn(100)
     } else {
       $(this)
         .closest('.top-search__input')
         .find('.clear')
-        .fadeOut(100);
+        .fadeOut(100)
     }
-  });
-  $('.modal').addClass('scroll-y');
+  })
+  $('.modal').addClass('scroll-y')
   // toggle account dropdown
   $('#toggleHeadDropdown').click(function() {
-    $('#headDropdown').slideToggle();
-    $('.sidebar').toggleClass('dropdown-active');
-  });
+    $('#headDropdown').slideToggle()
+    $('.sidebar').toggleClass('dropdown-active')
+  })
 
   // show change password inputs
   $('#btnChangePassword').on('click', function() {
     $(this)
       .closest('.password-wrap')
       .find('.input-field')
-      .fadeIn();
+      .fadeIn()
     $(this)
       .parent()
-      .animate({ height: 0, opacity: 0, margin: '-24px 0 0 0', width: 0 }, 300);
-  });
+      .animate({ height: 0, opacity: 0, margin: '-24px 0 0 0', width: 0 }, 300)
+  })
   // file upload with thumbnail
-});
+})
 
 function readURL(input) {
   if (input.files && input.files[0]) {
-    var reader = new FileReader();
+    var reader = new FileReader()
     reader.onload = function(e) {
-      $('.image-upload-wrap').hide();
-      $('.file-upload-image').attr('src', e.target.result);
-      $('.file-upload-content').show();
+      $('.image-upload-wrap').hide()
+      $('.file-upload-image').attr('src', e.target.result)
+      $('.file-upload-content').show()
 
       // $('.image-title').html(input.files[0].name);
-    };
+    }
 
-    reader.readAsDataURL(input.files[0]);
+    reader.readAsDataURL(input.files[0])
   } else {
-    removeUpload();
+    removeUpload()
   }
 }
 
 function removeUpload() {
-  $('.file-upload-input').replaceWith($('.file-upload-input').clone());
-  $('.file-upload-content').hide();
-  $('.image-upload-wrap').show();
+  $('.file-upload-input').replaceWith($('.file-upload-input').clone())
+  $('.file-upload-content').hide()
+  $('.image-upload-wrap').show()
 }
 
 // on page load...
-moveProgressBar();
+moveProgressBar()
 // on browser resize...
 $(window).resize(function() {
-  moveProgressBar();
-});
+  moveProgressBar()
+})
 
 // SIGNATURE PROGRESS
 function moveProgressBar() {
-  var getPercent = $('.progress-wrap').data('progress-percent') / 100;
-  var getProgressWrapWidth = $('.progress-wrap').width();
-  var progressTotal = getPercent * getProgressWrapWidth;
-  $('.progress-bar').width(progressTotal);
+  var getPercent = $('.progress-wrap').data('progress-percent') / 100
+  var getProgressWrapWidth = $('.progress-wrap').width()
+  var progressTotal = getPercent * getProgressWrapWidth
+  $('.progress-bar').width(progressTotal)
 }
 // Custom color picker
-var color_picker = document.querySelectorAll('.color-picker');
+var color_picker = document.querySelectorAll('.color-picker')
 color_picker.forEach(function(item) {
-  item.parentNode.style.backgroundColor = item.value;
+  item.parentNode.style.backgroundColor = item.value
   item.addEventListener('change', function() {
-    item.parentNode.style.backgroundColor = item.value;
-  });
-});
+    item.parentNode.style.backgroundColor = item.value
+  })
+})
 
 // Drag and Drop via Dragula https://github.com/bevacqua/dragula
-var dragContainers = document.querySelectorAll('.dragContainer');
+var dragContainers = document.querySelectorAll('.dragContainer')
 if (dragContainers.length > 0) {
-  let dragContainersArray = Array.from(dragContainers);
+  let dragContainersArray = Array.from(dragContainers)
   dragula(dragContainersArray)
     .on('drag', function(el) {
       // add 'is-moving' class to element being dragged
-      el.classList.add('is-moving');
+      el.classList.add('is-moving')
     })
     .on('dragend', function(el) {
       // remove 'is-moving' class from element after dragging has stopped
-      el.classList.remove('is-moving');
+      el.classList.remove('is-moving')
 
       // add the 'is-moved' class for 600ms then remove it
       window.setTimeout(function() {
-        el.classList.add('is-moved');
+        el.classList.add('is-moved')
         window.setTimeout(function() {
-          el.classList.remove('is-moved');
-        }, 600);
-      }, 100);
-    });
+          el.classList.remove('is-moved')
+        }, 600)
+      }, 100)
+    })
 }
 
 // Resize and Drag and Drop with interact.js http://interactjs.io/
@@ -131,13 +131,17 @@ if (document.querySelector('.resize-drag')) {
       restrictSize: {
         min: { width: 100, height: 50 }
       },
-
+      snap: {
+        targets: [interact.createSnapGrid({ x: 0, y: 0 })],
+        range: Infinity,
+        relativePoints: [{ x: 0, y: 0 }]
+      },
       inertia: true,
       // call this function on every dragmove event
       onmove: dragMoveListener,
       // call this function on every dragend event
       onend: function(event) {
-        var textEl = event.target.querySelector('p');
+        var textEl = event.target.querySelector('p')
 
         textEl &&
           (textEl.textContent =
@@ -147,53 +151,53 @@ if (document.querySelector('.resize-drag')) {
                 Math.pow(event.pageY - event.y0, 2)) |
                 0
             ).toFixed(2) +
-            'px');
+            'px')
       }
     })
     .on('resizemove', function(event) {
       var target = event.target,
         x = parseFloat(target.getAttribute('data-x')) || 0,
-        y = parseFloat(target.getAttribute('data-y')) || 0;
+        y = parseFloat(target.getAttribute('data-y')) || 0
 
       // update the element's style
-      target.style.width = event.rect.width + 'px';
-      target.style.height = event.rect.height + 'px';
+      target.style.width = event.rect.width + 'px'
+      target.style.height = event.rect.height + 'px'
 
       // translate when resizing from top or left edges
-      x += event.deltaRect.left;
-      y += event.deltaRect.top;
+      x += event.deltaRect.left
+      y += event.deltaRect.top
 
       target.style.webkitTransform = target.style.transform =
-        'translate(' + x + 'px,' + y + 'px)';
+        'translate(' + x + 'px,' + y + 'px)'
 
-      target.setAttribute('data-x', x);
-      target.setAttribute('data-y', y);
+      target.setAttribute('data-x', x)
+      target.setAttribute('data-y', y)
       target.textContent =
-        Math.round(event.rect.width) + '\u00D7' + Math.round(event.rect.height);
-    });
+        Math.round(event.rect.width) + '\u00D7' + Math.round(event.rect.height)
+    })
 }
 function dragMoveListener(event) {
   var target = event.target,
     // keep the dragged position in the data-x/data-y attributes
     x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
-    y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+    y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
 
   // translate the element
   target.style.webkitTransform = target.style.transform =
-    'translate(' + x + 'px, ' + y + 'px)';
+    'translate(' + x + 'px, ' + y + 'px)'
 
   // update the posiion attributes
-  target.setAttribute('data-x', x);
-  target.setAttribute('data-y', y);
+  target.setAttribute('data-x', x)
+  target.setAttribute('data-y', y)
 }
 
 // this is used later in the resizing and gesture demos
-window.dragMoveListener = dragMoveListener;
+window.dragMoveListener = dragMoveListener
 // end Resize and Drag and Drop with interact.js
 
 // Credit card validation
 /* Card.js plugin by Jesse Pollak. https://github.com/jessepollak/card */
-var $editCreditCard = $('.edit-credit-form');
+var $editCreditCard = $('.edit-credit-form')
 if ($editCreditCard) {
   $editCreditCard.card({
     container: '.card-wrapper',
@@ -201,5 +205,5 @@ if ($editCreditCard) {
     formSelectors: {
       nameInput: 'input[name="first-name"], input[name="last-name"]'
     }
-  });
+  })
 }
